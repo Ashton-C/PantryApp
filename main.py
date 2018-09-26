cc656973a407ebb031617b3b5c2e9881cd249ac7
ashton_c <armadillomaster@gmail.com>
2018-09-25 21:42:38 -0600

    DB skeleton setup!
    Got the SQLAlchemy database skeleton set up (thanks @CorySchafer). Pretty rough right now, will enhance later.

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    db.__tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    user_pantry = db.relationship('Pantry', backref='user', lazy=True)
+
+    def __repr__(self):
+        return "Username: {}, Email: {}".format(self.username, self.email)
+
+
+class Pantry(db.Model):
+    db.__tablename__ = "pantry"
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(40), nullable=False, unique=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    par = db.Column(db.Integer, nullable=True)
+    expiration = db.Column(db.DateTime, nullable=True, default='1/1/2049')
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 @app.route('/')
 def main():
-    return """
-    This is the homepage! Welcome to your pantry! Here you can add things to your pantry!
-    When adding things you can add the name, quantity, par, and expiration date. COOL NOW I HAVE TO LEARN ALL THE HTML/JINJA STUFF!
-    """
+    return "Please Login!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
